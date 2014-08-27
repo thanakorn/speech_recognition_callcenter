c33faef8ac180fcfb111b2a86ea5d64111354e5b
thanakorn <stylus_l@hotmail.com>
2014-08-27 23:18:51 +0700

    Create Postpaid model

---
+++ src/models/postpaid.py
@@ -0,0 +1,24 @@
+__author__ = 'thanakorn'
+
+
+class Postpaid(object):
+
+    def __init__(self, name, type, calling_time, extra_call_rate, internet_data, max_internet_speed, max_speed_data, overlimit_internet_speed, sms_rate, free_wifi, fee, payment):
+        self.name = name
+        self.type = type
+        self.calling_time = calling_time
+        self.extra_call_rate = extra_call_rate
+        self.internet_data = internet_data
+        self.max_internet_speed = max_internet_speed
+        self.max_speed_data = max_speed_data
+        self.overlimit_internet_speed = overlimit_internet_speed
+        self.sms_rate = sms_rate
+        self.free_wifi = free_wifi
+        self.fee = fee
+        self.payment = payment
+
+    def is_internet_unlimited(self):
+        return self.internet_data == -1
+
+    def is_unlimited_wifi(self):
+        return self.free_wifi == -1
