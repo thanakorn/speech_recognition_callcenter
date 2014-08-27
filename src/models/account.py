9987c942abd7c72d72ebdd23edc5ddbffe5c0616
thanakorn <stylus_l@hotmail.com>
2014-08-28 00:59:23 +0700

    Create account model

---
+++ src/models/account.py
@@ -0,0 +1,20 @@
+__author__ = 'thanakorn'
+
+from src.database import Database
+
+
+class Account(object):
+
+    db = Database()
+
+    def __init__(self, customer, package, balance, expiration_date):
+        self.customer = customer
+        self.package = package
+        self.balance = balance
+        self.expiration_date = expiration_date
+
+    @classmethod
+    def fromjson(cls, json):
+        customer = cls.db.find_by_id('customers', json['customer'])
+        package = cls.db.find_by_id('prepaid', json['package'])
+        cls(customer, package, json['balance'], json['expiration_date'])
