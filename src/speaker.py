99cc8707aa480ac86ad28ab8e4eef19c682c1884
thanakorn <stylus_l@hotmail.com>
2014-08-27 21:34:01 +0700

    Create speaker

---
+++ src/speaker.py
@@ -0,0 +1,13 @@
+__author__ = 'thanakorn'
+
+from subprocess import call
+
+
+class Speaker:
+
+    def __init__(self):
+        pass
+
+    @staticmethod
+    def speak(msg):
+        call(['sapitest', msg, '-v', 'Mary', '-s', '1.0'])
