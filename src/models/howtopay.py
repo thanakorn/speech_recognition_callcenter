64423c0f1472ab5575795582d5e83d1b90a02e7a
thanakorn <stylus_l@hotmail.com>
2014-09-11 08:32:55 +0700

    Create payment method

---
+++ src/models/howtopay.py
@@ -0,0 +1,14 @@
+__author__ = 'thanakorn'
+
+class HowToPay(object):
+
+    def __init__(self, id, method, step):
+        self.id = id
+        self.os = method
+        self.step = step
+
+    @classmethod
+    def fromjson(cls, json):
+        if json is None:
+            return None
+        return cls(json['_id'], json['method'], json['step'])
